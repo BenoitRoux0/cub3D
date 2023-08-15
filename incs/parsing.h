29ce3fb12d184c7b3bfc4a1827a3d11e9f4c3996
gd-harco <gd-harco@student.42lyon.fr>
2023-08-15 10:58:22 +0200

    Start of the work on parsing
    A new header file, parsing.h, was created to help organize and modularize the parsing functionality of the application. This file was included in the cub.h header to make the new parsing functions widely available across the codebase. The added files were also reflected in the Makefile and sources.mk. This change aims to enhance code readability and maintainability."

--- incs/parsing.h
+++ incs/parsing.h
@@ -1,25 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cub.h                                              :+:      :+:    :+:   */
+/*   parsing.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: beroux <beroux@student.42lyon.fr>          +#+  +:+       +#+        */
+/*   By: gd-harco <gd-harco@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/15 03:37:07 by beroux            #+#    #+#             */
-/*   Updated: 2023/08/15 09:10:47 by beroux           ###   ########.fr       */
+/*   Created: 2023/08/15 10:52:54 by gd-harco          #+#    #+#             */
+/*   Updated: 2023/08/15 10:52:54 by gd-harco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CUB_H
-# define CUB_H
+#ifndef PARSING_H
+# define PARSING_H
 
-# include <unistd.h>
-# include <stdlib.h>
-# include "mlx.h"
-# include "mlx_int.h"
-# include "types.h"
-# include "render.h"
-# include "hooks.h"
-# include "parsing.h"
+# include "cub.h"
 
 #endif

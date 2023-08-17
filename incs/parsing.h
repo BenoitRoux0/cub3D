881f68b4092b6983883f537f5f9f93e33240c81a
gd-harco <gd-harco@student.42lyon.fr>
2023-08-17 23:36:36 +0200

    Add error messages for invalid arguments in parsing.h
    The change introduces error messages for invalid command line arguments used in the cub3D project. The error messages include definitions for wrong usage, incorrect file extension, and inaccessible map file. This improves the traceability of error sources, aiding in future debugging efforts.

--- incs/parsing.h
+++ incs/parsing.h
@@ -6,7 +6,7 @@
 /*   By: gd-harco <gd-harco@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 10:52:54 by gd-harco          #+#    #+#             */
-/*   Updated: 2023/08/15 10:52:54 by gd-harco         ###   ########.fr       */
+/*   Updated: 2023/08/17 23:10:36 by gd-harco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,12 @@
 
 # include "cub.h"
 
+
+//-------Error Message-------//
+# define ERR_ARGC "cub3D: Usage: ./cub3D <map.cub>\n"
+# define ERC_ARGC 101
+# define ERR_EXTENSION "cub3D: Error: wrong map file extension, expeted <.cub>\n"
+# define ERC_EXTENSION 102
+# define ERR_ACCESS "cub3D: Error: can't access map file: "
+# define ERC_ACCESS 103
 #endif

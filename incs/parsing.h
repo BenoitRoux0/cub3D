c39c630b011c356aa39b4dd3cad70aa7a8fcd3d4
gd-harco <gd-harco@student.42lyon.fr>
2023-08-18 11:28:57 +0200

    Improved error handling
    The program will now start by launching the parsing code. If anything goes wrong during this step, the program now exit properly.

--- incs/parsing.h
+++ incs/parsing.h
@@ -6,7 +6,7 @@
 /*   By: gd-harco <gd-harco@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 10:52:54 by gd-harco          #+#    #+#             */
-/*   Updated: 2023/08/17 23:10:36 by gd-harco         ###   ########.fr       */
+/*   Updated: 2023/08/18 11:19:48 by gd-harco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,20 @@
 # include "cub.h"
 
 
+int	parsing(int argc, char **argv, t_data *data);
+
+
 //-------Error Message-------//
 # define ERR_ARGC "cub3D: Usage: ./cub3D <map.cub>\n"
 # define ERC_ARGC 101
-# define ERR_EXTENSION "cub3D: Error: wrong map file extension, expeted <.cub>\n"
+# define ERR_EXTENSION "cub3D: Error: wrong map file extension,\
+	expected <.cub>\n"
 # define ERC_EXTENSION 102
 # define ERR_ACCESS "cub3D: Error: can't access map file: "
 # define ERC_ACCESS 103
+# define ERR_TEXTURE_NORTH "cub3D: Error: can't access texture for north"
+# define ERR_TEXTURE_SOUTH "cub3D: Error: can't access texture for south"
+# define ERR_TEXTURE_EAST "cub3D: Error: can't access texture for east"
+# define ERR_TEXTURE_WEST "cub3D: Error: can't access texture for west"
+# define ERC_TEXTURE 104
 #endif
--- srcs/parsing/parsing.c
+++ srcs/parsing/parsing.c
@@ -6,15 +6,16 @@
 /*   By: gd-harco <gd-harco@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/17 22:36:45 by gd-harco          #+#    #+#             */
-/*   Updated: 2023/08/17 23:17:11 by gd-harco         ###   ########.fr       */
+/*   Updated: 2023/08/18 11:20:05 by gd-harco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub.h"
 
 bool	unvalid_extension(char *map);
+int		get_texture(int fd, t_data *data);
 
-int	parsing(int argc, char **argv)
+int	parsing(int argc, char **argv, t_data *data)
 {
 	int	fd;
 
@@ -22,9 +23,13 @@ int	parsing(int argc, char **argv)
 		return (printf(ERR_ARGC), ERC_ARGC);
 	if (unvalid_extension(argv[1]))
 		return (printf(ERR_EXTENSION), ERC_EXTENSION);
-	if (open(argv[1], O_RDONLY) == -1)
-		return (printf(ERR_ACCESS), ERC_ACCESS);
-	//TODO: open file, get texture, get map, get player
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+		return (printf(ERR_ACCESS"%s\n", argv[1]), ERC_ACCESS);
+	(void)data;
+//	if (get_texture(fd, data) == EXIT_FAILURE)
+//		return (ERC_TEXTURE);
+	//TODO: get map, get player
 	return (0);
 }
 

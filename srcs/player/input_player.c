0d572b4d0c45178cd4d030c275154fd9f7e3f5cf
beroux <42benoit.roux@gmail.com>
2023-08-29 17:12:10 +0200

    In the 'input_player.c' file, the conditions to alter the player's movements have been revised to correct the movement logic. This change was brought about due to incorrect movement directions - the keys mapped for each direction were leading to the opposite movements.
    In the 'update_player.c' file, refined the player's angle and position update logic by adding more conditions and calculations, involving the player's current angle and speed. Also, debug logs were commented out to clean up the console output when the game is running. This change was done to make the character's movements more precise and natural, depending on the speed and orientation.

--- srcs/player/input_player.c
+++ srcs/player/input_player.c
@@ -6,7 +6,7 @@
 /*   By: beroux <beroux@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 17:06:05 by beroux            #+#    #+#             */
-/*   Updated: 2023/08/18 19:01:49 by beroux           ###   ########.fr       */
+/*   Updated: 2023/08/21 15:10:28 by beroux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ int	key_press_player(int key_code, t_data *data)
 	if (key_code == XK_s)
 		data->player.mov[1] -= 1;
 	if (key_code == XK_q)
-		data->player.mov[0] -= 1;
-	if (key_code == XK_d)
 		data->player.mov[0] += 1;
+	if (key_code == XK_d)
+		data->player.mov[0] -= 1;
 	if (key_code == XK_Left)
 		data->player.angle_mov -= 1;
 	if (key_code == XK_Right)
@@ -36,9 +36,9 @@ int	key_released_player(int key_code, t_data *data)
 	if (key_code == XK_s)
 		data->player.mov[1] += 1;
 	if (key_code == XK_q)
-		data->player.mov[0] += 1;
-	if (key_code == XK_d)
 		data->player.mov[0] -= 1;
+	if (key_code == XK_d)
+		data->player.mov[0] += 1;
 	if (key_code == XK_Left)
 		data->player.angle_mov += 1;
 	if (key_code == XK_Right)
--- srcs/player/update_player.c
+++ srcs/player/update_player.c
@@ -6,7 +6,7 @@
 /*   By: beroux <beroux@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 17:08:12 by beroux            #+#    #+#             */
-/*   Updated: 2023/08/18 19:04:20 by beroux           ###   ########.fr       */
+/*   Updated: 2023/08/24 04:03:58 by beroux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,15 @@
 
 int	update_player(t_data *data)
 {
-	printf("update player\n");
-	data->player.angle += data->player.angle_mov;
-	printf("mov: %f %f\n", data->player.mov[0], data->player.mov[1]);
-	data->player.pos[0] += cos((data->player.angle - 90) * M_PI_4 / 45) * data->player.mov[0] - sin((data->player.angle - 90) * M_PI_4 / 45) * data->player.mov[1];
-	data->player.pos[1] += sin((data->player.angle - 90) * M_PI_4 / 45) * data->player.mov[0] + cos((data->player.angle - 90) * M_PI_4 / 45) * data->player.mov[1];
+//	printf("update player\n");
+	data->player.angle += data->player.angle_mov * PLAYER_SPEED;
+	data->player.angle = fmod(data->player.angle, 360);
+	if (data->player.angle < 0)
+		data->player.angle = 360 + data->player.angle;
+//	printf("mov: %f %f\n", data->player.mov[0], data->player.mov[1]);
+	data->player.pos[0] += (cos((data->player.angle - 90) * M_PI_4 / 45) * data->player.mov[0] - sin((data->player.angle - 90) * M_PI_4 / 45) * data->player.mov[1]) * PLAYER_SPEED;
+	data->player.pos[1] += (sin((data->player.angle - 90) * M_PI_4 / 45) * data->player.mov[0] + cos((data->player.angle - 90) * M_PI_4 / 45) * data->player.mov[1]) * PLAYER_SPEED;
+	if (data->player.angle_mov)
+		printf("player angle: %f\n", data->player.angle);
 	return (0);
 }

3de245ed46ac441e2c466836c46b58b70784b38e
beroux <42benoit.roux@gmail.com>
2023-08-31 14:30:17 +0200

    Correct the variable name and improve raycasting logic
    There is a correction in `draw.c` to modify a variable name (`img->line_length` to `img->line_len`). Additionally, there are various changes to the `raycast.c` file.
    
    The changes introduce a more robust ray casting logic. The adjustments include type changes to function parameters, math computation adjustments, code clean-ups, and a new function `select_ray()` to handle ray selection logic between horizontal and vertical intersections.
    
    These changes aim to improve the drawing and rendering capabilities and ensure the accuracy of the implementation. These will enhance the visual rendering and interaction within the game environment.

--- srcs/render/raycast.c
+++ srcs/render/raycast.c
@@ -6,19 +6,20 @@
 /*   By: beroux <beroux@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 14:28:49 by beroux            #+#    #+#             */
-/*   Updated: 2023/08/26 18:21:47 by beroux           ###   ########.fr       */
+/*   Updated: 2023/08/30 19:21:14 by beroux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub.h"
 
-typedef int (*t_collide_check_func)(double[2], t_ray, t_map);
+typedef int (*t_collide_check_func)(double[2], t_ray *, t_map);
 
 static int		cast_vert(double start[2], double angle, t_ray *ray, t_map map);
 static int		cast_horiz(double start[2], double angle, t_ray *ray, t_map map);
 static t_ray	dda(double start[2], double vector[2], t_map map, t_collide_check_func ray_collide);
-static int		ray_collide_horiz(double vector[2], t_ray ray, t_map map);
-static int		ray_collide_vert(double vector[2], t_ray ray, t_map map);
+static int		ray_collide_horiz(double vector[2], t_ray *ray, t_map map);
+static int		ray_collide_vert(double vector[2], t_ray *ray, t_map map);
+static t_ray	select_ray(t_ray inter_horiz, t_ray inter_vert);
 
 int	raycast(t_data *data)
 {
@@ -37,7 +38,7 @@ int	raycast(t_data *data)
 	{
 		cast_vert(data->player.pos, current_angle, &ray_vert, data->map);
 		cast_horiz(data->player.pos, current_angle, &ray_horiz, data->map);
-		data->rays[i] = ray_horiz;
+		data->rays[i] = select_ray(ray_horiz, ray_vert);
 		current_angle += data->player.fov / WIN_WIDTH;
 		current_angle = fmod((fmod(current_angle, 360) + 360), 360);
 		i++;
@@ -51,13 +52,14 @@ static int	cast_vert(double start[2], double angle, t_ray *ray, t_map map)
 	double	rad;
 	double 	vector[2];
 
-	angle = fmod(angle, 360);
 	rad = angle * M_PI_4 / 45;
-	inter_vert.inter[0] = ((double) CELL_SIZE) * ((int)(start[0] / CELL_SIZE) + (angle < 90 || angle > 270));
-	inter_vert.dist = fabs(inter_vert.inter[0] - start[0]) / cos(rad);
+	inter_vert.inter[0] = ((double) CELL_SIZE) * ((int)(start[0] / CELL_SIZE)/* + (angle < 90 || angle > 270)*/);
+	inter_vert.dist = (inter_vert.inter[0] - start[0]) / cos(rad);
 	inter_vert.inter[1] = start[1] + inter_vert.dist * sin(rad);
 	vector[0] =  CELL_SIZE - 2 * CELL_SIZE * (angle > 90 && angle < 270);
-	vector[1] = (CELL_SIZE / (start[0] - inter_vert.inter[0])) * (start[1] - inter_vert.inter[1]);
+	vector[1] = (CELL_SIZE / (start[0] - inter_vert.inter[0])) * (inter_vert.inter[1] - start[1]);
+	if (angle < 90 || angle > 270)
+		vector[1] *= -1;
 //	printf("vert vector %f %f\n", vector[0], vector[1]);
 	*ray = dda(inter_vert.inter, vector, map, ray_collide_vert);
 	ray->dist += sqrt(pow(start[0] - inter_vert.inter[0], 2) + pow(start[1] - inter_vert.inter[1], 2));
@@ -70,24 +72,19 @@ static int	cast_horiz(double start[2], double angle, t_ray *ray, t_map map)
 	double	rad;
 	double	vector[2];
 
-	(void) map;
-	(void) ray;
 	if (angle < 0 || angle > 360)
-		return (printf("angle error: %f\n", angle));
+		return (/*printf("angle error: %f\n", angle), */1);
 	rad = angle * M_PI_4 / 45;
-	inter_horiz.inter[1] = (double) CELL_SIZE * ((int)(start[1] / CELL_SIZE) - (angle < 180) - (fmod(start[1], CELL_SIZE) == 0 && angle > 180) + (fmod(start[1], CELL_SIZE) == 0 && angle <= 180));
+	inter_horiz.inter[1] = (double) CELL_SIZE * ((int)(start[1] / CELL_SIZE) - (fmod(start[1], CELL_SIZE) == 0 && angle > 180) + (fmod(start[1], CELL_SIZE) == 0 && angle <= 180));
 	inter_horiz.dist = (inter_horiz.inter[1] - start[1]) / sin(rad);
-	inter_horiz.inter[0] = fabs(inter_horiz.dist * cos(rad));
-	if (angle > 90 && angle < 270)
-		inter_horiz.inter[0] *= -1;
+	inter_horiz.inter[0] = (inter_horiz.dist * cos(rad));
 	inter_horiz.inter[0] += start[0];
+	if (angle > 180)
+		printf("angle > 180\n");
 	vector[1] = CELL_SIZE - 2 * CELL_SIZE * (angle > 180);
-	vector[0] = fabs((CELL_SIZE / (start[1] - inter_horiz.inter[1])) * (inter_horiz.inter[0] - start[0]));
-	if (angle > 90 && angle < 270)
+	vector[0] = (CELL_SIZE / (start[1] - inter_horiz.inter[1])) * (inter_horiz.inter[0] - start[0]);
+	if (angle < 180)
 		vector[0] *= -1;
-	if (vector[0] > 0)
-		printf("vector x > 0\n");
-//	printf("horiz vector: %f %f\n", vector[0], vector[1]);
 	*ray = dda(inter_horiz.inter, vector, map, ray_collide_horiz);
 	return (0);
 }
@@ -99,48 +96,59 @@ static t_ray	dda(double start[2], double vector[2], t_map map, t_collide_check_f
 	double	vec_magnitude;
 
 //	printf("vector: %f %f\n", vector[0], vector[1]);
+	ray.hit = 0;
 	nb_iter = 0;
 	ray.inter[0] = start[0];
 	ray.inter[1] = start[1];
 	ray.dist = 0;
-	hit = ray_collide(vector, ray, map);
+	hit = ray_collide(vector, &ray, map);
 	vec_magnitude = sqrt(vector[0] * vector[0] + vector[1] * vector[1]);
 	while (!hit && nb_iter < INTER_LIM)
 	{
 		ray.inter[0] += vector[0];
 		ray.inter[1] += vector[1];
 		ray.dist += vec_magnitude;
-		hit = ray_collide(vector, ray, map);
+		hit = ray_collide(vector, &ray, map);
 		nb_iter++;
 	}
 	return (ray);
 }
 
-static int	ray_collide_horiz(double vector[2], t_ray ray, t_map map)
+static int	ray_collide_horiz(double vector[2], t_ray *ray, t_map map)
 {
 	int	to_check[2];
 
-	(void) vector;
-	to_check[1] = (int) ray.inter[1] / CELL_SIZE - (vector[1] < 0);
-	to_check[0] = (int) (ray.inter[0]) / CELL_SIZE;// - (vector[0] < 0);
+	to_check[1] = (int) ray->inter[1] / CELL_SIZE - (vector[1] < 0);
+	to_check[0] = (int) (ray->inter[0]) / CELL_SIZE;// - (vector[0] < 0);
 	if (to_check[0] < 0 || to_check[0] >= map.size[0] || \
 		to_check[1] < 0 || to_check[1] >= map.size[1])
-		return (/*printf("horiz hit in %d %d\n", to_check[0], to_check[1]), */1);
+		return (/*printf("horiz hit in %d %d\n", to_check[0], to_check[1]), */ray->hit = 0, 1);
 	if (map.content[to_check[1]][to_check[0]] == '1')
-		return (/*printf("horiz hit in %d %d\n", to_check[0], to_check[1]), */1);
+		return (/*printf("horiz hit in %d %d\n", to_check[0], to_check[1]), */ray->hit = 1, 1);
 	return (0);
 }
 
-static int	ray_collide_vert(double vector[2], t_ray ray, t_map map)
+static int	ray_collide_vert(double vector[2], t_ray *ray, t_map map)
 {
 	int	to_check[2];
 
-	to_check[0] = (int) ray.inter[0] / CELL_SIZE - (vector[0] < 0);
-	to_check[1] = (int) ((ray.inter[1] + CELL_SIZE) / CELL_SIZE);
+	to_check[0] = (int) ray->inter[0] / CELL_SIZE - (vector[0] < 0);
+	to_check[1] = (int) ((ray->inter[1] + CELL_SIZE) / CELL_SIZE) - (vector[1] > 0);
 	if (to_check[0] < 0 || to_check[0] >= map.size[0] || \
-			to_check[1] < 0 || to_check[1] >= map.size[1])
-		return (/*printf("vert hit in %d %d\n", to_check[0], to_check[1]), */1);
+		to_check[1] < 0 || to_check[1] >= map.size[1])
+		return (/*printf("vert hit in %d %d\n", to_check[0], to_check[1]), */ray->hit = 0, 1);
 	if (map.content[to_check[1]][to_check[0]] == '1')
-		return (/*printf("vert hit in %d %d\n", to_check[0], to_check[1]), */1);
+		return (/*printf("vert hit in %d %d\n", to_check[0], to_check[1]), */ray->hit = 1, 1);
 	return (0);
+}
+
+static t_ray	select_ray(t_ray inter_horiz, t_ray inter_vert)
+{
+	if (inter_horiz.hit && !inter_vert.hit)
+		return (inter_horiz);
+	if (inter_vert.hit && !inter_horiz.hit)
+		return (inter_vert);
+	if (inter_horiz.dist < inter_vert.dist)
+		return (inter_horiz);
+	return (inter_vert);
 }

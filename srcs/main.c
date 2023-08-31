67a990a95a318c1c573a577410c6b11a2518d0de
beroux <42benoit.roux@gmail.com>
2023-08-31 17:42:58 +0200

    Refactor rendering and raycasting in main.c and raycast.c
    The modifications in `main.c` include:
    - Adding `img.h` to the import statements.
    - Adding static functions `draw_map`, `draw_player`, and `loop_hook`.
    - Updating the `main` function to use the new functions and initialize the necessary variables.
    
    The modifications in `raycast.c` include:
    - Changing the signature of the `dda` function to accept a `t_ray` struct as the starting point.
    - Making adjustments to the `cast_vert` and `cast_horiz` functions to use the new `dda` function.
    - Removing a commented out print statement in `ray_collide_vert`.
    - Adding a commented out return statement in `select_ray`.
    
    Note: The changes related to imports are ignored as per the instructions.

--- srcs/main.c
+++ srcs/main.c
@@ -6,39 +6,111 @@
 /*   By: beroux <beroux@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 03:40:16 by beroux            #+#    #+#             */
-/*   Updated: 2023/08/15 09:32:08 by beroux           ###   ########.fr       */
+/*   Updated: 2023/08/29 18:06:48 by beroux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub.h"
+#include "img.h"
+
+static int	draw_map(t_map map, t_uint_img *dst);
+static int	draw_player(t_player *player, t_uint_img *dst);
+static int	loop_hook(t_data *data);
 
 int	main(void)
 {
 	t_data	data;
-	int		start[2];
-	int		end[2];
-	t_line	line;
+	const char *map[] = {
+			"11111111111",
+			"10000100001",
+			"10000000001",
+			"10000000001",
+			"11111111111"
+	};
 
-	start[0] = 5;
-	start[1] = 5;
-	end[0] = 150;
-	end[1] = 150;
 	data.mlx = mlx_init();
 	if (!data.mlx)
 		return (1);
 	data.win = mlx_new_window(data.mlx, 200, 200, "cub3D");
 	if (!data.win)
 		return (2);
-	data.master_img.img_ptr = mlx_new_image(data.mlx, 200, 200);
-	line = create_line(start, end);
-	data.master_img.addr = mlx_get_data_addr(data.master_img.img_ptr,
-			&data.master_img.bits_per_pixel,
-			&data.master_img.line_length,
-			&data.master_img.endian);
-	nlx_draw_line(&data.master_img, &line, COLOR_WHITE);
-	mlx_put_image_to_window(data.mlx, data.win, data.master_img.img_ptr, 0, 0);
+	data.map.content = (char **) map;
+	data.map.size[0] = 11;
+	data.map.size[1] = 5;
+	data.player = init_player(NORTH, (int[2]) {3, 3});
+	mlx_do_key_autorepeatoff(data.mlx);
 	mlx_hook(data.win, DestroyNotify, NoEventMask, on_destroy, &data);
 	mlx_hook(data.win, KeyPress, KeyPressMask, on_key_press, &data);
+	mlx_hook(data.win, KeyRelease, KeyReleaseMask, on_key_released, &data);
+	mlx_loop_hook(data.mlx, loop_hook, &data);
 	mlx_loop(data.mlx);
 	return (1);
 }
+
+int	loop_hook(t_data *data)
+{
+	t_uint_img			*dst;
+	static t_master_img	*to_put = NULL;
+	int					i;
+
+	update_player(data);
+	raycast(data);
+	dst = init_img(WIN_WIDTH, WIN_HEIGHT);
+	draw_map(data->map, dst);
+	draw_player(&data->player, dst);
+	i = 0;
+	while (i < WIN_WIDTH)
+	{
+		if (data->rays[i].inter[0] < WIN_WIDTH && data->rays[i].inter[1] < WIN_HEIGHT && data->rays[i].inter[0] > 0 && data->rays[i].inter[1] > 0)
+		{
+			dst->content[(int) data->rays[i].inter[1]][(int) data->rays[i].inter[0]] = color_to_uint32_t((t_color) {255, 0, 255, 0});
+		}
+		i++;
+	}
+	img_to_mlx_img(data->mlx, &to_put, dst);
+	mlx_put_image_to_window(data->mlx, data->win, to_put->content, 0, 0);
+	clear_img(dst);
+	return (0);
+}
+
+int draw_map(t_map map, t_uint_img *dst)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < dst->height && i < map.size[1] * CELL_SIZE)
+	{
+		j = 0;
+		while (j < dst->width && j < map.size[0] * CELL_SIZE)
+		{
+			dst->content[i][j] = color_to_uint32_t((t_color) {255, 0, 0, 0});
+			if (map.content[i / CELL_SIZE][j / CELL_SIZE] == '1')
+				dst->content[i][j] = color_to_uint32_t((t_color) {255, 255, 255, 255});
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+int draw_player(t_player *player, t_uint_img *dst)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < dst->height)
+	{
+		j = 0;
+		while (j < dst->width)
+		{
+			if (pow(player->pos[0] - j, 2) + pow(player->pos[1] - i, 2) <= 25)
+				dst->content[i][j] = color_to_uint32_t((t_color) {255, 255, 0, 0});
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
--- srcs/render/raycast.c
+++ srcs/render/raycast.c
@@ -16,7 +16,7 @@ typedef int (*t_collide_check_func)(double[2], t_ray *, t_map);
 
 static int		cast_vert(double start[2], double angle, t_ray *ray, t_map map);
 static int		cast_horiz(double start[2], double angle, t_ray *ray, t_map map);
-static t_ray	dda(double start[2], double vector[2], t_map map, t_collide_check_func ray_collide);
+static t_ray	dda(t_ray start, double vector[2], t_map map, t_collide_check_func ray_collide);
 static int		ray_collide_horiz(double vector[2], t_ray *ray, t_map map);
 static int		ray_collide_vert(double vector[2], t_ray *ray, t_map map);
 static t_ray	select_ray(t_ray inter_horiz, t_ray inter_vert);
@@ -53,16 +53,16 @@ static int	cast_vert(double start[2], double angle, t_ray *ray, t_map map)
 	double 	vector[2];
 
 	rad = angle * M_PI_4 / 45;
-	inter_vert.inter[0] = ((double) CELL_SIZE) * ((int)(start[0] / CELL_SIZE)/* + (angle < 90 || angle > 270)*/);
+	inter_vert.inter[0] = ((double) CELL_SIZE) * ((int)(start[0] / CELL_SIZE) - (fmod(start[0], CELL_SIZE) == 0 && (angle < 90 || angle > 270)) + (fmod(start[0], CELL_SIZE) == 0 && (angle < 90 && angle > 270))/* + (angle < 90 || angle > 270)*/);
 	inter_vert.dist = (inter_vert.inter[0] - start[0]) / cos(rad);
-	inter_vert.inter[1] = start[1] + inter_vert.dist * sin(rad);
+	inter_vert.inter[1] = fabs(inter_vert.dist * sin(rad));
+	if (angle > 180)
+		inter_vert.inter[1] *= -1;
+	inter_vert.inter[1] += start[1];
 	vector[0] =  CELL_SIZE - 2 * CELL_SIZE * (angle > 90 && angle < 270);
 	vector[1] = (CELL_SIZE / (start[0] - inter_vert.inter[0])) * (inter_vert.inter[1] - start[1]);
-	if (angle < 90 || angle > 270)
-		vector[1] *= -1;
 //	printf("vert vector %f %f\n", vector[0], vector[1]);
-	*ray = dda(inter_vert.inter, vector, map, ray_collide_vert);
-	ray->dist += sqrt(pow(start[0] - inter_vert.inter[0], 2) + pow(start[1] - inter_vert.inter[1], 2));
+	*ray = dda(inter_vert, vector, map, ray_collide_vert);
 	return (0);
 }
 
@@ -79,28 +79,25 @@ static int	cast_horiz(double start[2], double angle, t_ray *ray, t_map map)
 	inter_horiz.dist = (inter_horiz.inter[1] - start[1]) / sin(rad);
 	inter_horiz.inter[0] = (inter_horiz.dist * cos(rad));
 	inter_horiz.inter[0] += start[0];
-	if (angle > 180)
-		printf("angle > 180\n");
+//	if (angle > 180)
+//		printf("angle > 180\n");
 	vector[1] = CELL_SIZE - 2 * CELL_SIZE * (angle > 180);
 	vector[0] = (CELL_SIZE / (start[1] - inter_horiz.inter[1])) * (inter_horiz.inter[0] - start[0]);
 	if (angle < 180)
 		vector[0] *= -1;
-	*ray = dda(inter_horiz.inter, vector, map, ray_collide_horiz);
+	*ray = dda(inter_horiz, vector, map, ray_collide_horiz);
 	return (0);
 }
-static t_ray	dda(double start[2], double vector[2], t_map map, t_collide_check_func ray_collide)
+static t_ray	dda(t_ray ray, double vector[2], t_map map, t_collide_check_func ray_collide)
 {
 	int		nb_iter;
 	int		hit;
-	t_ray	ray;
 	double	vec_magnitude;
 
 //	printf("vector: %f %f\n", vector[0], vector[1]);
 	ray.hit = 0;
 	nb_iter = 0;
-	ray.inter[0] = start[0];
-	ray.inter[1] = start[1];
-	ray.dist = 0;
+	ray.dist = fabs(ray.dist);
 	hit = ray_collide(vector, &ray, map);
 	vec_magnitude = sqrt(vector[0] * vector[0] + vector[1] * vector[1]);
 	while (!hit && nb_iter < INTER_LIM)
@@ -133,7 +130,7 @@ static int	ray_collide_vert(double vector[2], t_ray *ray, t_map map)
 	int	to_check[2];
 
 	to_check[0] = (int) ray->inter[0] / CELL_SIZE - (vector[0] < 0);
-	to_check[1] = (int) ((ray->inter[1] + CELL_SIZE) / CELL_SIZE) - (vector[1] > 0);
+	to_check[1] = (int) ((ray->inter[1] + CELL_SIZE) / CELL_SIZE) - 1;
 	if (to_check[0] < 0 || to_check[0] >= map.size[0] || \
 		to_check[1] < 0 || to_check[1] >= map.size[1])
 		return (/*printf("vert hit in %d %d\n", to_check[0], to_check[1]), */ray->hit = 0, 1);
@@ -144,6 +141,7 @@ static int	ray_collide_vert(double vector[2], t_ray *ray, t_map map)
 
 static t_ray	select_ray(t_ray inter_horiz, t_ray inter_vert)
 {
+//	return (inter_vert);
 	if (inter_horiz.hit && !inter_vert.hit)
 		return (inter_horiz);
 	if (inter_vert.hit && !inter_horiz.hit)

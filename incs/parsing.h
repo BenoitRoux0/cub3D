d2f41c8538955673391f25ddb571ff37c42834bd
gd-harco <gd-harco@student.42lyon.fr>
2023-09-05 16:57:56 +0200

    Refactor texture and color parsing functionality
    This commit refactors the 'get_textures_and_colors' methods in 'parsing.c' and 'get_textures.c' to remove unnecessary 'map_path' argument, improving code readability. The error handling within these methods was also refined to provide more granular and informative error codes. Additionally, the return type of 'ft_xpm_to_img' method is updated in 'img.h'. These changes are made to simplify the code and improve error reporting capability.

--- incs/parsing.h
+++ incs/parsing.h
@@ -6,7 +6,7 @@
 /*   By: gd-harco <gd-harco@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 10:52:54 by gd-harco          #+#    #+#             */
-/*   Updated: 2023/09/02 14:21:46 by gd-harco         ###   ########.fr       */
+/*   Updated: 2023/09/05 16:52:41 by gd-harco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 
 //-----------------FUNCTION-----------------//
 int		parsing(int argc, char **argv, t_data *data);
-int		get_textures_and_colors(int fd, t_data *data, char *map_path);
+int		get_textures_and_colors(int fd, t_data *data);
 
 void	flush_newline(char *array[4]);
 void	free_textures_line(char *textures_line[4]);
@@ -27,22 +27,25 @@ void	get_textures_line(char *buff, char *line_array[4], int *data_got);
 int		get_color(char *buff, int color[2][3], int *data_got);
 
 //-----------------ERROR_MESSAGES-----------------//
-# define ERM_ARGC "cub3D: Usage: ./cub3D <map.cub>\n"
+# define ERM_ARGC "\033[1;31mcub3D: Usage: ./cub3D <map.cub>\n\033[0m"
 # define ERC_ARGC 101
-# define ERM_EXTENSION "cub3D: Error: wrong map file extension,\
-	expected <.cub>\n"
+# define ERM_EXTENSION "\033[1;31mcub3D: Error: wrong map file extension,\
+	expected <.cub>\n\033[0m"
 # define ERC_EXTENSION 102
-# define ERM_ACCESS "cub3D: Error: can't access map file: "
+# define ERM_ACCESS "\033[1;31mcub3D: Error: can't access map file: \033[0m"
 # define ERC_ACCESS 103
-# define ERM_TEXTURE_NORTH "cub3D: Error: can't access texture for north"
-# define ERM_TEXTURE_SOUTH "cub3D: Error: can't access texture for south"
-# define ERM_TEXTURE_EAST "cub3D: Error: can't access texture for east"
-# define ERM_TEXTURE_WEST "cub3D: Error: can't access texture for west"
+# define ERM_TEXTURE_NORTH "\033[1;31mcub3D: Error: can't access \
+texture for north\033[0m"
+# define ERM_TEXTURE_SOUTH "\033[1;31mcub3D: Error: can't access texture for south\033[0m"
+# define ERM_TEXTURE_EAST "\033[1;31mcub3D: Error: can't access \
+texture for east\033[0m"
+# define ERM_TEXTURE_WEST "\033[1;31mcub3D: Error: can't access \
+texture for west\033[0m"
 # define ERC_TEXTURE 104
-# define ERM_MISS_TEXTURE "cub3D: Error: missing texture path"
-# define ERC_MISS_TEXTURE 105
-# define ERM_ARRAY_BIGGER "cub3D: Error: to many info on line "
+# define ERM_NB_INFO "\033[1;31mcub3D: Error: wrong number of info in provided file\033[0m"
+# define ERC_NB_INFO 105
+# define ERM_ARRAY_BIGGER "\033[1;31mcub3D: Error: to many info on line \033[0m"
 # define ERC_ARRAY_BIGGER 106
-# define ERM_OOR_VALUE "cub3D: Error: color value out of range"
+# define ERM_OOR_VALUE "\033[1;31mcub3D: Error: color value out of range\033[0m"
 # define ERC_OOR_VALUE 107
 #endif

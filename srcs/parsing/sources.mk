2dda577e9533dbbe3982e0f0444efcbf48554ceb
gd-harco <gd-harco@student.42lyon.fr>
2023-09-09 18:32:25 +0200

    Added "check_map.c"

--- srcs/parsing/sources.mk
+++ srcs/parsing/sources.mk
@@ -2,6 +2,7 @@ SRCS +=	parsing/parsing.c				\
 		parsing/get_textures.c			\
 		parsing/get_textures_utils.c	\
 		parsing/get_color.c				\
-		parsing/read_map.c
+		parsing/read_map.c				\
+		parsing/check_map.c
 
 INCS +=	incs/parsing.h

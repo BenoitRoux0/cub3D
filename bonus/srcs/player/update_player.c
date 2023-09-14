3ab1bce14cb98dc30682201e8f75af3102bb01df
beroux <42benoit.roux@gmail.com>
2023-09-14 18:06:59 +0200

    Implement player collision with map boundaries
    Updated the 'update_player' functionality to include a collision check before updating player position. A helper function 'collide' was added, which accepts current and proposed next positions, and alters next positions if a collision with the edge of the map ('1' in map.content) is detected. This enhancement prevents the player from moving through walls, enhancing gameplay realism.

--- bonus/srcs/player/update_player.c
+++ bonus/srcs/player/update_player.c
@@ -6,27 +6,51 @@
 /*   By: beroux <beroux@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 17:08:12 by beroux            #+#    #+#             */
-/*   Updated: 2023/09/03 03:52:11 by beroux           ###   ########.fr       */
+/*   Updated: 2023/09/14 18:03:13 by beroux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub.h"
 
+static void	collide(t_data *data, double pos[2], double next_pos[2]);
+
 int	update_player(t_data *data)
 {
+	double	next_pos[2];
+
 	data->player.angle += data->player.angle_mov * PLAYER_SPEED;
 	data->player.angle = fmod(data->player.angle, 360);
 	if (data->player.angle < 0)
 		data->player.angle = 360 + data->player.angle;
-	data->player.pos[0] += (cos((data->player.angle - 90) * M_PI_4 / 45) * \
+	next_pos[0] = data->player.pos[0] + \
+							(cos((data->player.angle - 90) * M_PI_4 / 45) * \
 														data->player.mov[0] - \
 							sin((data->player.angle - 90) * M_PI_4 / 45) * \
 														data->player.mov[1]) * \
 																PLAYER_SPEED;
-	data->player.pos[1] += (sin((data->player.angle - 90) * M_PI_4 / 45) * \
+	next_pos[1] = data->player.pos[1] + \
+							(sin((data->player.angle - 90) * M_PI_4 / 45) * \
 														data->player.mov[0] + \
 							cos((data->player.angle - 90) * M_PI_4 / 45) * \
 														data->player.mov[1]) * \
 																PLAYER_SPEED;
+	collide(data, data->player.pos, next_pos);
+	data->player.pos[0] = next_pos[0];
+	data->player.pos[1] = next_pos[1];
 	return (0);
+}
+
+static void	collide(t_data *data, double pos[2], double next_pos[2])
+{
+	int pos_in_map[2];
+	int	next_pos_in_map[2];
+
+	pos_in_map[0] = (int) (pos[0] / CELL_SIZE);
+	pos_in_map[1] = (int) (pos[1] / CELL_SIZE);
+	next_pos_in_map[0] = (int) (next_pos[0] / CELL_SIZE);
+	next_pos_in_map[1] = (int) (next_pos[1] / CELL_SIZE);
+	if (data->map.content[pos_in_map[1]][next_pos_in_map[0]] == '1')
+		next_pos[0] = pos[0];
+	if (data->map.content[next_pos_in_map[1]][pos_in_map[0]] == '1')
+		next_pos[1] = pos[1];
 }
